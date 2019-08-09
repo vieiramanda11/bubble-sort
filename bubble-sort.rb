b0ee2516a55287a2f30122f8e9bdb450d3e7d67d
vieiramanda11 <amanda_vieira@outlook.com>
2019-08-09 14:08:39 -0300

    adding bubble sort by method

--- bubble-sort.rb
+++ bubble-sort.rb
@@ -1,14 +1,30 @@
 # bubble sort method
 def bubble_sort(array)
-    switch = true
-    while switch
-      switch = false
-      (0...array.length - 1).each do |i|  
-        if array[i] > array[i + 1] # if this condition is not true switch maintains false
-          array[i], array[i + 1] = array[i + 1], array[i]
-          switch = true # loop again
-        end 
+  switch = true
+  while switch
+    switch = false
+    (0...array.length - 1).each do |i|  
+      if array[i] > array[i + 1] # if this condition is not true switch maintains false
+        array[i], array[i + 1] = array[i + 1], array[i]
+        switch = true # loop again
+      end 
+    end
+  end
+  array
+end
+
+# bubble sort by method
+
+def bubble_sort_by(array)
+  switch = true
+  while switch
+    switch = false
+    (0...array.length - 1).each do |i|
+      if yield(array[i], array[i + 1]).positive? 
+        array[i], array[i + 1] = array[i + 1], array[i]
+        switch = true
       end
     end
-    array
   end
+  array
+end
