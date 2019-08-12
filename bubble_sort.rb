8a569c32a785a665817971248cd8397366e0c79b
vieiramanda11 <amanda_vieira@outlook.com>
2019-08-12 11:18:35 -0300

    fixing linters errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 # bubble sort method
 def bubble_sort(array)
   switch = true
   while switch
     switch = false
-    (0...array.length - 1).each do |i|  
+    (0...array.length - 1).each do |i|
       if array[i] > array[i + 1] # if this condition is not true switch maintains false
         array[i], array[i + 1] = array[i + 1], array[i]
         switch = true # loop again
-      end 
+      end
     end
   end
   array
@@ -20,7 +22,7 @@ def bubble_sort_by(array)
   while switch
     switch = false
     (0...array.length - 1).each do |i|
-      if yield(array[i], array[i + 1]).positive? 
+      if yield(array[i], array[i + 1]).positive?
         array[i], array[i + 1] = array[i + 1], array[i]
         switch = true
       end
@@ -29,7 +31,7 @@ def bubble_sort_by(array)
   array
 end
 
-# tests 
-puts bubble_sort([4,3,78,2,0,2])
+# tests
+puts bubble_sort([4, 3, 78, 2, 0, 2])
 puts ' '
-puts bubble_sort_by(["hi","hello","hey"]) { |left,right| left.length - right.length }
+puts bubble_sort_by(%w[hi hello hey]) { |left,right| left.length - right.length }
